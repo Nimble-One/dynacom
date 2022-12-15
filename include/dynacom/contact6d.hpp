4142402f9ed89cbaa3227a28950f9152047fb69a
Nahuel Villa <nvilla@laas.fr>
2022-12-15 17:56:42 +0100

    documentation completed, comments removed.

--- include/dynacom/contact6d.hpp
+++ include/dynacom/contact6d.hpp
@@ -81,59 +81,6 @@ class Contact6D : public ContactBase {
   std::string &getFrameName() { return settings_.frame_name; }
 };
 
-// struct ContactPointSettings {
-//  public:
-//   double mu;
-//   Eigen::Matrix<double, 3, 1> weights;
-//   std::string frame_name;
-
-//   std::string to_string() {
-//     std::ostringstream out;
-//     out << "Contact6D "
-//         << ":\n";
-//     out << "    mu: " << this->mu << "\n";
-//     out << "    weights: " << this->weights.transpose() << std::endl;
-
-//     return out.str();
-//   }
-
-//   std::ostream &operator<<(std::ostream &out) {
-//     out << this->to_string();
-//     return out;
-//   }
-
-//   bool operator==(const ContactPointSettings &rhs) {
-//     bool test = true;
-//     test &= this->frame_name == rhs.frame_name;
-//     test &= this->mu == rhs.mu;
-//     test &= this->weights == rhs.weights;
-//     return test;
-//   }
-
-//   bool operator!=(const ContactPointSettings &rhs) { return !(*this == rhs);
-//   }
-// };
-
-// class ContactPoint : public ContactBase {
-//  private:
-//   ContactPointSettings settings_;
-
-//  public:
-//   ContactPoint();
-//   ContactPoint(const ContactPointSettings &settings);
-//   void initialize(const ContactPointSettings &settings);
-
-//   // setters
-//   void setMu(const double &mu);
-//   void setForceWeights(const Eigen::Vector3d &force_weights);
-//   void updateNewtonEuler(const Eigen::Vector3d &CoM, const pinocchio::SE3
-//   &oMf);
-
-//   // getters
-//   const ContactPointSettings &getSettings() { return settings_; }
-//   std::string &getFrameName() { return settings_.frame_name; }
-// };
-
 }  // namespace dynacom
 
 #endif  // DYNACOM_CONTACT_6D
